0e5e4db93494369abd9d1531d59409a68bbc1caa
Marcus Ikegami <hikegami.6@gmail.com>
2021-12-05 17:52:03 -0700

    Added all post routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,6 +5,7 @@ router.get('/', (req, res) => {
     console.log('=================');
     Post.findAll({
         attributes: ['id', 'post_url', 'title', 'created_at'],
+        order: [['created_at', 'DESC']],
         include: [
             {
                 model: User,
@@ -13,6 +14,90 @@ router.get('/', (req, res) => {
         ]
     })
         .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(postData => {
+            if(!postData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(postData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/', (req, res) => {
+     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+     Post.create({
+         title: req.body.title,
+         post_url: req.body.post_url,
+         user_id: req.body.user_id
+     })
+        .then(postData => res.json(postData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/:id', (req, res) => {
+
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+        .then(postData => {
+            if(!postData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(postData);
+        })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+});
+
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(postData => {
+            if(!postData) {
+                res.status(404).json({ message: 'No post found with this id'});
+                return;
+            }
+            res.json(postData);
+        }) 
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
